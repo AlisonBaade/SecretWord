0b67e706d62e6391a81d51337b94e8d45270da93
Alison Baade <alison_baade@hotmail.com>
2023-05-31 00:12:21 -0300

    projeto para treino de react

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordList = {
+    carro: ["Motor", "Porta", "Volante", "Pneu", "Antena"],
+    fruta:["Banana","Melancia","Abacaxi","Laranja","Ameixa"],
+    corpo:["Perna","Quadril","Dedo","Olhos","Nariz"],
+    computador:["Mouse","Teclado","Monitor","Gabinete"],
+    programação:["Linguagem","Framework","JavaScript","React"],
+    alimento:["Arroz","Carne","Leite","Ovo","Pizza"],
+};
